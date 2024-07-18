4671e18f45bff4c95609b173e2ca47bb8db509f4
johonnes <wjohonnes@gmail.com>
2024-07-18 19:29:19 -0300

    correção do programa MeuPrimeiroProgramaJava

--- src/edu/dio/java/MeuPrimeiroProgramaJava/MeuPrimeiroProgramaJava.java
+++ src/edu/dio/java/MeuPrimeiroProgramaJava/MeuPrimeiroProgramaJava.java
@@ -1,4 +1,5 @@
-public class App {
+package edu.dio.java.MeuPrimeiroProgramaJava;
+public class MeuPrimeiroProgramaJava {
     public static void main(String[] args) throws Exception {
         System.out.println("olá, World!");
     }
