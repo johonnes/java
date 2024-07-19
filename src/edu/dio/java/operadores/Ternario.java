762cd8ef4e4d750803d0f70653e280b0cda80234
johonnes <wjohonnes@gmail.com>
2024-07-18 22:14:20 -0300

    exemplos de ternário e relacional

---
+++ src/edu/dio/java/operadores/Ternario.java
@@ -0,0 +1,25 @@
+package edu.dio.java.operadores;
+
+public class Ternario {
+    public static void main(String[] args) {
+        // classe Operadores.java
+int a, b;
+
+a = 6;
+b = 6;
+
+//EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+String resultado1 = "";
+if(++a==b)
+   resultado1 = "verdadeiro";
+else
+   resultado1 = "falso";
+
+   System.out.println(resultado1);
+
+//MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+int resultado = (--a==b) ? 1 : 0 ;
+
+System.out.println(resultado);
+    }
+}
