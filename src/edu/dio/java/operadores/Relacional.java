fd80b313f3974745ee44c3f10701b5bf60aa6b97
johonnes <wjohonnes@gmail.com>
2024-07-18 22:21:40 -0300

    atualização do exemplo relacional

--- src/edu/dio/java/operadores/Relacional.java
+++ src/edu/dio/java/operadores/Relacional.java
@@ -20,5 +20,27 @@ if(numero1 <= numero2)
 
 if(numero1 != numero2)
 	System.out.print("Numero 1 é diferente de numero 2");  
-    }
+
+    // ComparacaoAvancada.java
+
+    String nome1 = "JAVA";
+    String nome2 = "JAVA";
+    
+    System.out.println(nome1 == nome2); //true
+
+    String nome3 = new String("JAVA");
+    
+    System.out.println(nome1 == nome3); //false
+
+    String nome4 = nome3;
+
+    System.out.println(nome3 == nome4); //true
+    
+    //equals na parada
+    System.out.println(nome1.equals(nome2)); //??
+    System.out.println(nome2.equals(nome3)); //??
+    System.out.println(nome3.equals(nome4)); //??
+
 }
+    }
+
