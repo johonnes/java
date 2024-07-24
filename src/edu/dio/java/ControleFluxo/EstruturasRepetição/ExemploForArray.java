0d6749dcdcf99d5f646f8b607d6456de82bba341
johonnes <wjohonnes@gmail.com>
2024-07-24 19:53:27 -0300

    exemplo de forArray

---
+++ src/edu/dio/java/ControleFluxo/EstruturasRepetição/ExemploForArray.java
@@ -0,0 +1,12 @@
+package edu.dio.java.ControleFluxo.EstruturasRepetição;
+
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+    
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    
+    }
+}
