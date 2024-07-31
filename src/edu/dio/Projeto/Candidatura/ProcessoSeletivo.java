c5e21d844980667052fb5ad5bcff7d31bd7e4003
johonnes <wjohonnes@gmail.com>
2024-07-31 15:18:36 -0300

    exemplos de exceptions e do desafio

---
+++ src/edu/dio/Projeto/Candidatura/ProcessoSeletivo.java
@@ -0,0 +1,41 @@
+package edu.dio.Projeto.Candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        selecaoCandidatos();
+      
+    }
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato "+ candidato +" Solicitou este valor de salário %.2f"+ salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato "+ candidato +" foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+
+            candidatosAtual++;
+        }
+    }
+    static double valorPretendido() {
+     return ThreadLocalRandom.current().nextDouble(1800, 2200);
+}
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if(salarioBase==salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }else{
+            System.out.println("AGUARDANDO O RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+    }
+}
---
+++ src/edu/dio/Projeto/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+package edu.dio.Projeto.DesafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    
+}
